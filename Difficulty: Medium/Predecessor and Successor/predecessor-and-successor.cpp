fc612eb2fc9498546e2e01e738fda58888e8d26e
Brijkishor636 <kbrijkishor636@gmail.com>
2025-11-10 20:58:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -0,0 +1,44 @@
+/* BST Node
+class Node {
+   public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node *pre = NULL;
+        Node *suc = NULL;
+        Node *curr = root;
+        while(curr != NULL){
+            if(curr->data < key){
+                pre = curr;
+                curr = curr->right;
+            }
+            else{
+                curr = curr->left;
+            }
+        }
+        
+        curr = root;
+        while(curr != NULL){
+            if(curr->data > key){
+                suc = curr;
+                curr = curr->left;
+            }
+            else{
+                curr = curr->right;
+            }
+        }
+        return {pre, suc};
+    }
+};
