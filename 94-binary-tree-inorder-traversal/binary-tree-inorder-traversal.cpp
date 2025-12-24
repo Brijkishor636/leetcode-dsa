9b5a89498750f06e400f5aeb1370debd46931ec4
Brijkishor636 <kbrijkishor636@gmail.com>
2025-12-24 17:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (32.81%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,18 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void inOrder(TreeNode *root, vector<int>&result){
+//         if(root == NULL)
+//             return;
+//         inOrder(root->left, result);
+//         result.push_back(root->val);
+//         inOrder(root->right, result);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int>result;
+//         inOrder(root, result);
+//         return result;
+//     }
+// };
+
+
+// Using stack ---->
+
 class Solution {
 public:
-    void inOrder(TreeNode *root, vector<int>&result){
-        if(root == NULL)
-            return;
-        inOrder(root->left, result);
-        result.push_back(root->val);
-        inOrder(root->right, result);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>result;
-        inOrder(root, result);
+        if(root == NULL){
+            return result;
+        }
+        stack<TreeNode*>st;
+        while(root != NULL || !st.empty()){
+            while(root != NULL){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            result.push_back(root->val);
+            root = root->right;
+        }
         return result;
     }
 };
