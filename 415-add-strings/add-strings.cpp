be9975fd4f9c35d2a61a133aab86eabdf15a09ac
Brijkishor636 <kbrijkishor636@gmail.com>
2026-02-04 00:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (58.35%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        // int sum = stoll(num1) + stoll(num2);
+        // return to_string(sum);
+        string ans = "";
+        int carry = 0;
+        int i = num1.size() - 1, j = num2.size() - 1;
+        while(i >= 0 || j >= 0 || carry){
+            int sum = carry;
+            if(i >= 0){
+                sum += num1[i--] - '0';
+            }
+            if(j >= 0) sum += num2[j--] - '0';
+            ans.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
