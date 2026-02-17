f0338b2874274010fc4700e54179dd66b70e00de
Brijkishor636 <kbrijkishor636@gmail.com>
2026-02-17 16:44:56 +0530

    Time: 158 ms (33.71%) | Memory: 21.5 MB (31.42%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>words(wordList.begin(), wordList.end());
+        unordered_set<string>visited;
+        queue<string>q;
+        q.push(beginWord);
+        int level = 1;
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                string temp = q.front();
+                q.pop();
+                if(temp == endWord){
+                    return level;
+                }
+                for(char ch: "abcdefghijklmnopqrstuvwxyz"){
+                    for(int i = 0; i < temp.size(); i++){
+                        string s = temp;
+                        s[i] = ch;
+
+                        if(visited.find(s) == visited.end() && words.find(s) != words.end()){
+                            visited.insert(s);
+                            q.push(s);
+                            words.erase(s);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+};
