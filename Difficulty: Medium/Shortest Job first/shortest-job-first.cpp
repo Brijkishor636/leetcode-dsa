e09d76939f8c1d5413ac70ff0f3231d8c3bd071b
Brijkishor636 <kbrijkishor636@gmail.com>
2026-09-24 07:08:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -1,17 +1,32 @@
+// class Solution {
+//   public:
+//     int solve(vector<int>& bt) {
+//         int n = bt.size();
+//         sort(bt.begin(), bt.end());
+//         vector<int>prefix(n, 0);
+//         prefix[0] = bt[0];
+//         for(int i = 1; i < n; i++){
+//             prefix[i] = bt[i] + prefix[i-1];
+//         }
+//         int sum = 0;
+//         for(int i = 0; i < n-1; i++){
+//             sum += prefix[i];
+//         }
+//         return sum / n;
+//     }
+// };
+
+
 class Solution {
   public:
     int solve(vector<int>& bt) {
         int n = bt.size();
         sort(bt.begin(), bt.end());
-        vector<int>prefix(n, 0);
-        prefix[0] = bt[0];
-        for(int i = 1; i < n; i++){
-            prefix[i] = bt[i] + prefix[i-1];
+        int t = 0, wt = 0;
+        for(int i = 0; i < n; i++){
+            wt += t;
+            t += bt[i];
         }
-        int sum = 0;
-        for(int i = 0; i < n-1; i++){
-            sum += prefix[i];
-        }
-        return sum / n;
+        return (wt/n);
     }
 };
